9a98c3d5af3e5caa1764809218879b1849ccdb3d
Edgardl De la Hoz <edelahoz@ulearning.com.co>
2025-04-25 00:23:35 -0500

    feat: ruta dinamica por productId realizada y funcional.

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -9,38 +9,44 @@ const products = () => {
   const [loading, setLoading] = React.useState(true);
 
   useEffect(() => {
-      const fetchProducts = async () => {
-        try {
-          const productsRef = collection(db, 'products');
-          const q = query(productsRef); // Limitar a 8 productos destacados
-          const querySnapshot = await getDocs(q);
-          const productsList = querySnapshot.docs.map(doc => ({
-            id: doc.id,
-            ...doc.data()
-          }));
-          setProducts(productsList);
-        } catch (error) {
-          console.error('Error al obtener productos:', error);
-        } finally {
-          setLoading(false);
-        }
-      };
-  
-      fetchProducts();
-    }, []);
-  
+    const fetchProducts = async () => {
+      try {
+        const productsRef = collection(db, "products");
+        const q = query(productsRef);
+        const querySnapshot = await getDocs(q);
+        const productsList = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setProducts(productsList);
+      } catch (error) {
+        console.error("Error al obtener productos:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div className="mx-auto py-8 my-10 flex flex-col gap-12">
-      <h2 className='text-4xl text-center text-[#1C2838]'>Todos los Productos</h2>
-      <div className='flex justify-center'>
-        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6'>
-          { loading ? <p>Cargando productos...</p> 
-          : products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          )) }
-        </div>
+      <h2 className="text-4xl text-center text-[#1C2838]">
+        Todos los Productos
+      </h2>
+      <div className="flex justify-center">
+        {loading ? (
+          <div className="flex justify-center items-center h-64">
+            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-[#1C2838]"></div>
+          </div>
+        ) : (
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+            {products.map((product) => (
+              <ProductCard key={product.id} product={product}/>
+            ))}
+          </div>
+        )}
       </div>
-      
     </div>
   );
 };
