e3ebec3447ae794d5892445fb6d8a62389ac3095
Edgardl De la Hoz <edelahoz@ulearning.com.co>
2025-04-25 19:16:10 -0500

    feat: add sweetalert2 for enhanced user notifications and loading spinner in App component implement lazy loading for components and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,43 @@
-import './App.css'
-import Header from './components/layouts/header'
-import Footer from './components/layouts/footer'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/home'
-import Login from './components/auth/login'
-import Register from './components/auth/register'
-import Products from './pages/products'
-import Cart from './pages/cart'
-import Admin from './pages/admin'
-import ProductPage from './pages/product-page'
+import "./App.css";
+import Header from "./components/layouts/header";
+import Footer from "./components/layouts/footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import SpinnerLoading from "./components/utils/SpinnerLoading";
+
+// Importación de las páginas principales
+const Home = lazy(() => import("./pages/home"));
+const Login = lazy(() => import("./components/auth/login"));
+const Register = lazy(() => import("./components/auth/register"));
+
+// Importación de las páginas relacionadas con productos
+const Products = lazy(() => import("./pages/products"));
+const ProductPage = lazy(() => import("./pages/product-page"));
+
+// Importación de las páginas relacionadas con el carrito y el administrador
+const Cart = lazy(() => import("./pages/cart"));
+const Admin = lazy(() => import("./pages/admin"));
 
 function App() {
   return (
     <Router>
+      <Suspense fallback={<SpinnerLoading />}>
       <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/products'>
-          <Route index element={<Products />} />
-          <Route path=':id' element={<ProductPage />} />
-        </Route>
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/admin' element={<Admin />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/products">
+            <Route index element={<Products />} />
+            <Route path=":id" element={<ProductPage />} />
+          </Route>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
       <Footer />
+      </Suspense>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
